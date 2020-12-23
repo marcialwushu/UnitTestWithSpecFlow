d9a56588b9ed3e46e879e2407d8df368ac772902
Cleilson <marcialwushu@gmail.com>
2020-12-23 00:39:35 -0300

    Initial commit

---
+++ API.Test/Cenarios/API/Requisicao.feature
@@ -0,0 +1,9 @@
+﻿#language: pt-br
+
+Funcionalidade: Sendo que sou um consumidor da API 
+	Gostaria de tesar as funcionalidades da API para Clientes
+
+Cenario: Obter informação da API
+	Dado que a url do endpoint é 'https://mock-api-collection.herokuapp.com/dados'
+	E o metodo http é 'GET'
+	Quando chamar o serviço
---
+++ API.Test/Cenarios/Steps/APISteps.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp;
+using System;
+using TechTalk.SpecFlow;
+
+namespace API.Test.Cenarios.Steps
+{
+    [Binding]
+    public class APISteps
+    {
+        [Given(@"que a url do endpoint é '(.*)'")]
+        [Obsolete]
+        public void DadoQueAUrlDoEndpointE(string url)
+        {
+            ScenarioContext.Current["Endpoint"] = url;
+        }
+        
+        [Given(@"o metodo http é '(.*)'")]
+        [Obsolete]
+        public void DadoOMetodoHttpE(string p0)
+        {
+            var metodo = Method.POST;
+
+            switch (p0.ToUpper())
+            {
+                case "POST":
+                    metodo = Method.POST;
+                    break;
+                case "GET":
+                    metodo = Method.GET;
+                    break;
+                case "PUT":
+                    metodo = Method.PUT;
+                    break;
+                case "DELETE":
+                    metodo = Method.DELETE;
+                    break;
+                case "PATCH":
+                    metodo = Method.PATCH;
+                    break;
+                default:
+                    Assert.Fail("Método HTTP não esperado");
+                    break;
+            }
+
+            ScenarioContext.Current["HttpMethod"] = metodo;
+        }
+
+        [When(@"chamar o serviço")]
+        [Obsolete]
+        public void QuandoChamarOServico()
+        {
+            var endpoint = (String)ScenarioContext.Current["Endpoint"];
+
+            ExecutarRequest(endpoint);
+        }
+
+        #region Private
+        [Obsolete]
+        private void ExecutarRequest(string endpoint)
+        {
+            var url = endpoint;
+
+            var request = new RestRequest();
+
+            request.Method = (Method)ScenarioContext.Current["HttpMethod"];
+
+            request.Parameters.Clear();
+        }
+        #endregion
+    }
+}
